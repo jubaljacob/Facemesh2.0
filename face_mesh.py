bd08e2877941b516b69cbd1e4d81994219b5c017
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-12-26 11:41:07 +0530

    module

--- face_mesh.py
+++ face_mesh.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture("Apple — Inclusion & Diversity — Open.mp4")
+cap = cv2.VideoCapture(0)
 pTime = 0
 cTime = 0
 mpDraw = mp.solutions.drawing_utils
